ba4d88d1c0d2fbce2ff2f503e009b6a229fd7f43
Hossein Narimani Rad <hossein.narimani.rad@gmail.com>
2025-12-13 16:33:40 +0330

    refactor(test): esrishape to geojson

--- tests/IRI.Maptor.Tst.Main/Esri/EsriShape_GeoJsonTest.cs
+++ tests/IRI.Maptor.Tst.Main/Esri/EsriShape_GeoJsonTest.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.SqlServer.Types;
-
-using IRI.Maptor.Extensions; 
-using IRI.Maptor.Tst.Assets;
+﻿using IRI.Maptor.Extensions;
+using IRI.Maptor.Sta.Spatial.Primitives;
+using IRI.Maptor.Sta.Common.Primitives;
+using IRI.Maptor.Sta.Spatial.IO.OgcSFA;
 
 namespace IRI.Maptor.Tst.Main.Esri;
 
@@ -11,35 +11,31 @@ public class EsriShape_GeoJsonTest
     {
         //SqlServerTypes.Utilities.LoadNativeAssembliesv14();
     }
-     
-    [Fact]
-    public void TestEsriShape_GeoJsonConversion()
-    { 
-        CheckGeoJsonEsriShapeConversion(SqlGeometrySamples.Point);
-        CheckGeoJsonEsriShapeConversion(SqlGeometrySamples.Multipoint);
-        CheckGeoJsonEsriShapeConversion(SqlGeometrySamples.Linestring);
-        CheckGeoJsonEsriShapeConversion(SqlGeometrySamples.MultiLineString);
-        CheckGeoJsonEsriShapeConversion(SqlGeometrySamples.Polygon);
-        CheckGeoJsonEsriShapeConversion(SqlGeometrySamples.PolygonWithHole);
-        CheckGeoJsonEsriShapeConversion(SqlGeometrySamples.PolygonWithTwoHole);
-        CheckGeoJsonEsriShapeConversion(SqlGeometrySamples.MultiPolygon01);
-        CheckGeoJsonEsriShapeConversion(SqlGeometrySamples.MultiPolygon02);
-    }
 
-    private void CheckGeoJsonEsriShapeConversion(SqlGeometry sqlGeometry)
+    [Theory]
+    [InlineData("Point(1 2)")]
+    [InlineData("MULTIPOINT((2 3), (7 8))")]
+    [InlineData("LINESTRING(1 1, 2 0,  2 4, 3 3)")]
+    [InlineData("MULTILINESTRING((1 1, 3 5), (-5 3, -8 -2))")]
+    [InlineData("POLYGON((0 0 9, 30 0 9, 30 30 9, 0 30 9, 0 0 9))")]
+    [InlineData("POLYGON((-20 -20, 20 -20, 20 20, -20 20, -20 -20), (10 0, 0 -10, 0 10, 10 0))")]
+    [InlineData("POLYGON((-20 -20, 20 -20, 20 20, -20 20, -20 -20), (10 0, 0 -10, 0 10, 10 0), (-10 0, -15 0, -10 10, -10 0))")]
+    [InlineData("MULTIPOLYGON(((0 0, 3 0, 3 3, 0 3, 0 0), (2 1, 1 1, 1 2, 2 1)), ((9 9, 10 9, 9 10, 9 9)))")]
+    [InlineData("MULTIPOLYGON(((0 0, 6 0, 6 6, 0 6, 0 0), (1 5, 5 5, 5 1, 1 1, 1 5)), ((4 4, 2 4, 2 2, 4 2, 4 4),(3.5 3.5, 3.5 2.5, 2.5 2.5, 2.5 3.5, 3.5 3.5)))")]
+    public void TestEsriShape_GeoJsonConversion(string wkt)
     {
-        var geoJson1 = sqlGeometry.AsGeoJson();
-        var geoJson2 = sqlGeometry.AsGeometry().AsGeoJson();
+        // Arrange
+        var geometry = WktReader.Parse(wkt) as Geometry<Point>;
 
-        Assert.Equal(geoJson1.NumberOfGeometries(), geoJson2.NumberOfGeometries());
-        Assert.Equal(geoJson1.NumberOfPoints(), geoJson2.NumberOfPoints());
-        Assert.Equal(geoJson1.Serialize(false), geoJson2.Serialize(false));
+        // Act - Path1: Geometry -> GeoJson -> EsriShape -> WKT
+        var esriShape1 = geometry.AsGeoJson().AsEsriShape(); 
+        var wkt1 = esriShape1.AsSqlServerWkt();
 
-        var esriShape1 = sqlGeometry.AsEsriShape();
-        var esriShape2 = sqlGeometry.AsGeoJson().AsEsriShape();
+        // Act - Path2: Geometry -> EsriShape -> WKT
+        var esriShape2 = geometry.AsEsriShape();
+        var wkt2 = esriShape2.AsSqlServerWkt();
 
-        Assert.Equal(esriShape1.AsSqlServerWkt(), esriShape2.AsSqlServerWkt());
-
+        // Assert
+        Assert.Equal(wkt1, wkt2);
     }
-
 }
